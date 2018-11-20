ced84a3a70e8d558094bd72fbc5718f07c46f419
César Nascimento <ceesar90@gmail.com>
2018-11-20 15:16:53 -0200

    Create opAritmeticas.cpp

---
+++ exercicios-pi/opAritmeticas.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+ 
+using std::cout;
+using std::cin;
+using std::endl;
+ 
+int main()
+{
+    int number1;
+    int number2;
+    cout << "Digite o numero 1: ";
+    cin >> number1 >> number2;
+ 
+    cout << "Soma: " << number1 + number2 << endl;
+    cout << "Produto: " << number1 * number2 << endl;
+    cout << "Diferença: " << number1 - number2 << endl;
+    cout << "Quociente: " << number1 / number2 << endl;
+    return 0;
+}
+//pag 53
